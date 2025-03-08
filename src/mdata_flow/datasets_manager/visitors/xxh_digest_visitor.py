a0382a1dc254b40e6d63a90b4dfada7ff3829efe
INovikov Home <nov.ilucha@yandex.ru>
2025-03-08 10:16:23 +0300

    now xxhash visitor support nested groups and it tested

--- src/mdata_flow/datasets_manager/visitors/xxh_digest_visitor.py
+++ src/mdata_flow/datasets_manager/visitors/xxh_digest_visitor.py
@@ -1,3 +1,5 @@
+from collections.abc import Iterator
+from contextlib import contextmanager
 from io import BufferedIOBase
 
 import xxhash
@@ -5,11 +7,24 @@ from typing_extensions import override
 
 from mdata_flow.datasets_manager.composites import GroupDataset, PdDataset
 from mdata_flow.datasets_manager.visitors.typed_abs_visitor import TypedDatasetVisitor
+from mdata_flow.types import NestedDict
 
 
 class XXHDigestDatasetVisitor(TypedDatasetVisitor):
-    _results: dict[str, str] = {}
-    _current_type: str | None = None
+    _results: NestedDict
+    # ссылка на текущий корень обработки
+    _results_tmp_link: NestedDict
+    # список ключей текущего уровня
+    _current_ds_key_path: list[str]
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._results = {}
+        self._results_tmp_link = self._results
+        self._current_ds_key_path = []
+
+    def get_results(self):
+        return self._results
 
     @staticmethod
     def _compute_xxhash(file: str | BufferedIOBase):
@@ -29,12 +44,43 @@ class XXHDigestDatasetVisitor(TypedDatasetVisitor):
     def VisitPdDataset(self, elem: PdDataset):
         digest = self._compute_xxhash(elem.temp_path)
         elem.digest = digest
-        if not self._current_type:
-            return
-        self._results.update({self._current_type: digest})
+
+        if not len(self._current_ds_key_path):
+            raise RuntimeError("Run without GroupDataset")
+
+        # забираем текущий ключ из списка и по нему назначаем
+        # результат
+        try:
+            key = self._current_ds_key_path[-1]
+            self._results_tmp_link.update({key: digest})
+        except KeyError as e:
+            e.add_note("Bad keys list")
+            raise
+
+    @contextmanager
+    def _manage_path(self) -> Iterator[None]:
+        backup_tmp_link = self._results_tmp_link
+        if len(self._current_ds_key_path):
+            # если путь не пустой, значит вызваны из группы
+            self._results_tmp_link.update({self._current_ds_key_path[-1]: {}})
+            tmp_link = self._results_tmp_link[self._current_ds_key_path[-1]]
+            if not isinstance(tmp_link, dict):
+                raise RuntimeError("Bad tmp_link XXHDigestDatasetVisitor")
+
+            # переносим ссылку на новую вложенность
+            self._results_tmp_link = tmp_link
+
+        yield
+
+        if len(self._current_ds_key_path):
+            self._results_tmp_link = backup_tmp_link
 
     @override
     def VisitGroupDataset(self, elem: GroupDataset):
-        for ds_type, value in elem.datasets.items():
-            self._current_type = ds_type
-            value.Accept(visitor=self)
+        with self._manage_path():
+            for value in elem.datasets:
+                # добавляем ключ датасета, в который заходить будем
+                self._current_ds_key_path.append(value.name)
+                value.Accept(visitor=self)
+                # извлекаем ключ, не нужен
+                _ = self._current_ds_key_path.pop()
---
+++ tests/datasets_manager/visitors/test_xxh_digest_visitor.py
@@ -0,0 +1,87 @@
+from unittest.mock import patch
+
+import pandas as pd
+import pytest
+
+from mdata_flow.datasets_manager.composites import GroupDataset, PdDataset
+from mdata_flow.datasets_manager.visitors import XXHDigestDatasetVisitor
+from mdata_flow.types import NestedDict
+
+
+def fake_xxhash(file):
+    return f"fake_hash_{file}" if isinstance(file, str) else "fake_hash_buffer"
+
+
+dataset1 = PdDataset(
+    name="file1.csv", dataset=pd.DataFrame(data=[1, 2], columns=pd.Index(["1"]))
+)
+dataset1.temp_path = dataset1.name
+dataset2 = PdDataset(
+    name="file2.csv", dataset=pd.DataFrame(data=[1, 2], columns=pd.Index(["1"]))
+)
+dataset2.temp_path = dataset2.name
+dataset3 = PdDataset(
+    name="file3.csv", dataset=pd.DataFrame(data=[1, 2], columns=pd.Index(["1"]))
+)
+dataset3.temp_path = dataset3.name
+
+subgroup2 = GroupDataset(name="subgroup2", datasets=[dataset1])
+subgroup = GroupDataset(name="subgroup", datasets=[subgroup2, dataset1, dataset2])
+root_group = GroupDataset(
+    name="root",
+    datasets=[GroupDataset(name="subgroup", datasets=[dataset1, dataset2]), dataset3],
+)
+root2_group = GroupDataset(
+    name="root",
+    datasets=[subgroup, dataset3],
+)
+
+
+@pytest.mark.parametrize(
+    "in_composite, expected_result",
+    [
+        pytest.param(
+            GroupDataset(name="root", datasets=[dataset1, dataset2, dataset3]),
+            {
+                "file1.csv": "fake_hash_file1.csv",
+                "file2.csv": "fake_hash_file2.csv",
+                "file3.csv": "fake_hash_file3.csv",
+            },
+            id="test_xxh_digest_visitor_flat",
+        ),
+        pytest.param(
+            root_group,
+            {
+                "subgroup": {
+                    "file1.csv": "fake_hash_file1.csv",
+                    "file2.csv": "fake_hash_file2.csv",
+                },
+                "file3.csv": "fake_hash_file3.csv",
+            },
+            id="test_xxh_digest_visitor_nested",
+        ),
+        pytest.param(
+            root2_group,
+            {
+                "subgroup": {
+                    "subgroup2": {
+                        "file1.csv": "fake_hash_file1.csv",
+                    },
+                    "file1.csv": "fake_hash_file1.csv",
+                    "file2.csv": "fake_hash_file2.csv",
+                },
+                "file3.csv": "fake_hash_file3.csv",
+            },
+            id="test_xxh_digest_visitor_flat",
+        ),
+    ],
+)
+def test_xxhdigest_nested(in_composite: GroupDataset, expected_result: NestedDict):
+    visitor = XXHDigestDatasetVisitor()
+
+    with patch.object(
+        XXHDigestDatasetVisitor, "_compute_xxhash", side_effect=fake_xxhash
+    ):
+        in_composite.Accept(visitor)
+    res = visitor.get_results()
+    assert res == expected_result
