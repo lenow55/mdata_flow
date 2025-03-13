564886e45f24d5d9adbb5c56de9519fa8bc5d891
INovikov Home <nov.ilucha@yandex.ru>
2025-03-13 21:03:47 +0300

    fix csv mover test for work multiple times

--- tests/datasets_manager/visitors/test_cache_mover_visitor.py
+++ tests/datasets_manager/visitors/test_cache_mover_visitor.py
@@ -1,7 +1,8 @@
-from collections.abc import Generator
 import os
+from collections import Counter
+from collections.abc import Generator
 from pathlib import Path
-from tempfile import NamedTemporaryFile, TemporaryDirectory, TemporaryFile
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 
 import pandas as pd
 import pytest
@@ -11,42 +12,17 @@ from mdata_flow.datasets_manager.visitors import CacheMoverDatasetVisitor
 from mdata_flow.types import NestedDict
 
 
-dataset1 = PdDataset(
-    name="file1", dataset=pd.DataFrame(data=[1, 2], columns=pd.Index(["1"]))
-)
-dataset1.temp_path = NamedTemporaryFile(delete=False).name
-dataset1.digest = dataset1.name
-dataset1.file_type = "csv"
-dataset2 = PdDataset(
-    name="file2", dataset=pd.DataFrame(data=[1, 2], columns=pd.Index(["1"]))
-)
-dataset2.temp_path = NamedTemporaryFile(delete=False).name
-dataset2.digest = dataset2.name
-dataset2.file_type = "csv"
-dataset3 = PdDataset(
-    name="file3", dataset=pd.DataFrame(data=[1, 2], columns=pd.Index(["1"]))
-)
-dataset3.temp_path = NamedTemporaryFile(delete=False).name
-dataset3.file_type = "csv"
-dataset3.digest = dataset3.name
+def create_dataset(name: str) -> PdDataset:
+    dataset = PdDataset(
+        name=name, dataset=pd.DataFrame(data=[1, 2], columns=pd.Index(["1"]))
+    )
+    dataset.temp_path = NamedTemporaryFile(delete=False).name
+    dataset.digest = dataset.name
+    dataset.file_type = "csv"
+    return dataset
 
-subgroup2 = GroupDataset(name="subgroup2", datasets=[dataset1])
-subgroup = GroupDataset(
-    name="subgroup",
-    datasets=[
-        subgroup2,
-        dataset1,
-        dataset2,
-    ],
-)
-root_group = GroupDataset(
-    name="root",
-    datasets=[GroupDataset(name="subgroup", datasets=[dataset1, dataset2]), dataset3],
-)
-root2_group = GroupDataset(
-    name="root",
-    datasets=[subgroup, dataset3],
-)
+
+dataset1 = create_dataset("file1")
 
 
 @pytest.fixture()
@@ -55,19 +31,22 @@ def cache_dir() -> Generator[str, None, None]:
         yield tempdir
 
 
-# FIXME: надо перенести инициализацию датасетов в другое место
-# чтобы временные файлы создавались каждый раз по новому
-
-
 @pytest.mark.parametrize(
     "in_composite, expected_result, expected_dir_content, run_name",
     [
         pytest.param(
-            GroupDataset(name="root", datasets=[dataset1, dataset2, dataset3]),
+            GroupDataset(
+                name="root",
+                datasets=[
+                    create_dataset("file1"),
+                    create_dataset("file2"),
+                    create_dataset("file3"),
+                ],
+            ),
             {
-                "file1": "test_run1\\file1.csv",
-                "file2": "test_run1\\file2.csv",
-                "file3": "test_run1\\file3.csv",
+                "file1": "test_run1/file1.csv",
+                "file2": "test_run1/file2.csv",
+                "file3": "test_run1/file3.csv",
             },
             [
                 "file1.csv",
@@ -78,7 +57,20 @@ def cache_dir() -> Generator[str, None, None]:
             id="test_cache_mover_visitor_flat",
         ),
         pytest.param(
-            root2_group,
+            GroupDataset(
+                name="root",
+                datasets=[
+                    GroupDataset(
+                        name="subgroup",
+                        datasets=[
+                            GroupDataset(name="subgroup2", datasets=[dataset1]),
+                            dataset1,
+                            create_dataset("file2"),
+                        ],
+                    ),
+                    create_dataset("file3"),
+                ],
+            ),
             {
                 "subgroup": {
                     "subgroup2": {
@@ -125,5 +117,5 @@ def test_cache_mover_visitor(
     list_files = os.listdir(os.path.join(cache_dir, run_name))
     assert len(list_runs) == 1
     assert list_runs[0] == run_name
-    assert list_files == expected_dir_content
+    assert Counter(list_files) == Counter(expected_dir_content)
     assert res == expected_result
