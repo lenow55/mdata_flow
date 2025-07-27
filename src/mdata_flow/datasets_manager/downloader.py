2678277873436d843d496feb99b1db43a2707eb2
Новиков Илья <nov.ilucha@yandex.ru>
2025-07-27 11:56:40 +0300

    update downloader for pandas production
    Взяты наработки с eis anomaly detection
    - теперь датасеты сразу в pandas dataframe загружаются
    - без указания версии извлекаются все уникальные датасеты
    последних версий
    - при указании имени датасета извлекается его последняя версия
    - при указании версии извлекаются датасеты только из этой версии рана

--- src/mdata_flow/datasets_manager/downloader.py
+++ src/mdata_flow/datasets_manager/downloader.py
@@ -1,9 +1,12 @@
 import os
+from collections.abc import Hashable, Mapping, Sequence
 from functools import partial
 
+import pandas as pd
 from mlflow.client import MlflowClient
 from mlflow.data import get_source
 from mlflow.data.filesystem_dataset_source import FileSystemDatasetSource
+from mlflow.data.pandas_dataset import PandasDataset
 from mlflow.entities import Run
 from mlflow.store.artifact.artifact_repository_registry import (
     _artifact_repository_registry,  # pyright: ignore[reportPrivateUsage]
@@ -44,8 +47,16 @@ class DatasetDownloader:
             os.mkdir(self.config.local_cache)
 
     def download(
-        self, run_name: str, run_version: int = -1, dataset_name: str | None = None
-    ) -> dict[str, str]:
+        self,
+        run_name: str,
+        run_version: int = -1,
+        dataset_name: str | None = None,
+        parse_dates: bool
+        | list[int]
+        | list[str]
+        | Sequence[Sequence[int]]
+        | Mapping[str, Sequence[int | str]] = False,
+    ) -> dict[str, PandasDataset]:
         if not self._experiment_id:
             raise RuntimeError("Run setup first")
 
@@ -61,24 +72,43 @@ class DatasetDownloader:
             order_by=["tags.version DESC"],
         )
 
-        try:
-            run = runs[0]
-        except IndexError:
-            return {}
+        result: dict[str, PandasDataset] = {}
 
-        result: dict[str, str] = {}
-        for dataset_input in run.inputs.dataset_inputs:
-            if dataset_name and dataset_name != dataset_input.dataset.name:
-                continue
-            source = get_source(dataset_input)
-            if not isinstance(source, FileSystemDatasetSource):
-                continue
-            if not isinstance(source.uri, str):
-                continue
-            file_name: str = os.path.basename(source.uri)
-            cache_file = os.path.join(self.config.local_cache, run_name, file_name)
-            if not os.path.exists(cache_file):
-                local_dataset_source = source.load(dst_path=os.path.dirname(cache_file))
-                cache_file = local_dataset_source
-            result.update({dataset_input.dataset.name: cache_file})
+        for run in runs:
+            for dataset_input in run.inputs.dataset_inputs:
+                # если установлено имя датасета, то все несовпадающие пропускаем
+                input_dataset_name = dataset_input.dataset.name
+                if dataset_name and dataset_name != input_dataset_name:
+                    continue
+                # если этого датасета ещё нет, то будем добавлять в результат,
+                # в итоге получим все датасеты последней версии
+                if input_dataset_name in result:
+                    continue
+
+                source = get_source(dataset_input)
+                if not isinstance(source, FileSystemDatasetSource):
+                    continue
+                if not isinstance(source.uri, str):  # pyright: ignore[reportUnnecessaryIsInstance]
+                    continue
+                file_name: str = os.path.basename(source.uri)
+                cache_file = os.path.join(self.config.local_cache, run_name, file_name)
+                if not os.path.exists(cache_file):
+                    local_dataset_source = source.load(
+                        dst_path=os.path.dirname(cache_file)
+                    )
+                    cache_file = local_dataset_source
+                dataset = PandasDataset(
+                    df=pd.read_csv(
+                        cache_file,
+                        index_col=0,
+                        parse_dates=parse_dates,
+                        compression="infer",
+                    ),
+                    source=source,
+                    digest=dataset_input.dataset.digest,
+                    name=dataset_input.dataset.name,
+                )
+
+                result.update({dataset_input.dataset.name: dataset})
+
         return result
